f00df34e44671ce9880366dfa7ef6f93d05c69c7
NikitaVrnk <nikitavrnk@gmail.com>
2022-01-27 13:28:49 +0300

    инициализация проекта + верстка

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import * as flsFunctions from './modules/functions.js'
+
+flsFunctions.isWebp()
+
+import Swiper, { Navigation, Pagination } from 'swiper'
+
+const swiper = new Swiper()
+
+
+
+let catalog = {
+    
+}
